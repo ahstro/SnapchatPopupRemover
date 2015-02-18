21b74f0bacaa9649b2f70a50d4d91cd31e63b145
LumenTeun <anton@lutic.org>
2015-02-18 22:44:33 +0100

    Initial commit.

---
+++ app/src/main/java/org/lutic/snapchatpopupremover/Main.java
@@ -0,0 +1,26 @@
+package org.lutic.snapchatpopupremover;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+
+public class Main implements IXposedHookLoadPackage {
+
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.snapchat.android")) {
+            return;
+        }
+
+        // Affectively disables the getErrorDialog function by returning
+        // '0' which is the SUCCESS constant for isGooglePlayServicesAvailable()
+        // Probably very bad since the app might think you actually have the Google services.
+        XposedHelpers.findAndHookMethod(
+                "com.google.android.gms.common.GooglePlayServicesUtil", lpparam.classLoader,
+                "isGooglePlayServicesAvailable", "android.content.Context",
+                XC_MethodReplacement.returnConstant(0)
+        );
+
+    }
+}
